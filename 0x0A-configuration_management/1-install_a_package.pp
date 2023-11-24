df08c38a72925cedc0d89824b4fbf08168002de8
Iwuchukwu Alexander <iwuchukwualexander@gmail.com>
2023-11-24 22:26:27 +0000

    Updated Config files

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 #!/usr/bin/pup
 # Using Puppt to install flask from pip3
-package {'flask':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
 
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
